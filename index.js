727e8da8178ac259b5d5b87b4437c95fd403f4fb
Rishab Parekh <rishabparekh14@gmail.com>
2023-07-20 07:44:08 -0700

    Backend of whatsapp bot

---
+++ index.js
@@ -0,0 +1,178 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const reader = require('xlsx');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "insurance"
+});
+
+app.listen(4000, ()=>{
+    console.log("running server");
+});
+
+
+app.post("/login", (req, res) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.query(
+        "SELECT * from users WHERE email = ? AND password = ?",
+        [email, password],
+        (err,result) =>{
+            if (err){
+                console.log("SOmething is wrong");
+                res.send({err: err})
+            }
+            if (result.length === 1){
+                console.log(result);
+                res.send(result);
+            }else{
+                console.log("Password Wrong");
+                res.send({message: "Wrong username/ password... Please try again"});
+            }
+        });
+});
+
+
+
+app.get('/customerData', (req, res) => {
+    const query = 'SELECT * from customers';
+
+    db.query(query, (error, results) => {
+        if(error) {
+            console.error('Error Executing the query:', error);
+            res.status(500).json({error: 'An error occurred'});
+            return;
+        }
+        res.json(results);
+    });
+});
+
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    console.log("File Data:"+req.file);
+    if (!req.file) {
+      res.status(400).json({ error: 'No file uploaded' });
+    } else {
+      // Access the uploaded file using req.file
+      const workbook = reader.readFile(req.file.path);
+
+  // Do your processing with the workbook here...
+  // For example, you can retrieve data from a specific sheet
+  console.log(workbook.length);
+  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+  const data = reader.utils.sheet_to_json(worksheet);
+  console.log("Data:",data, data.length);
+  data.forEach((row) => {
+    console.log("StartDate:",row['StartDate']);
+    const query = 'INSERT INTO customers (insured_name, phone_number, policy_number, NRMR_name, relationship_name, model_number, make, engine_number, chasis_number, SM_Name, Executive_Name, Executive_Number, start_date, end_Date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    db.query(query, [row['INSURED_NAME'], row['Mobile Number'], row['POLICYNO'], row['NRMR_Name'], row['RelationshipName'], row['MODEL_NO'], row['MAKE'], row['EngineNo'], row['ChassisNo'], row['SM_Name'], row['Executive Name'], row['Executive Number'], row['StartDate'], row['EndDate']], (err, result) => {
+      if (err) {
+        console.error('Error inserting row:', err);
+        return;
+      }
+      console.log('Row inserted:', result);
+    });
+  });
+
+      console.log('Uploaded file:', req.file);
+      res.json({ message: 'File uploaded successfully' });
+    }
+  });
+
+
+
+
+  app.get('/expiredPolicy', (req, res) => {
+    const query =  `
+    SELECT * FROM customers
+    WHERE end_date BETWEEN DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND CURDATE()
+  `;
+
+    db.query(query, (error, results) => {
+        if(error) {
+            console.error('Error Executing the query:', error);
+            res.status(500).json({error: 'An error occurred'});
+            return;
+        }
+        console.log('Results', results);
+        res.json(results);
+    });
+});
+
+app.post("/sendNotification", (req, res) =>{
+    console.log(req.body.customerData.insured_name);
+    // db.query(
+    //     "SELECT * from users WHERE email = ? AND password = ?",
+    //     [email, password],
+    //     (err,result) =>{
+    //         if (err){
+    //             console.log("SOmething is wrong");
+    //             res.send({err: err})
+    //         }
+    //         if (result.length === 1){
+    //             console.log(result);
+    //             res.send(result);
+    //         }else{
+    //             console.log("Password Wrong");
+    //             res.send({message: "Wrong username/ password... Please try again"});
+    //         }
+    //     });
+
+        const axios = require('axios');
+        let data = JSON.stringify({
+        "messaging_product": "whatsapp",
+        "to": req.body.customerData.phone_number,
+        "type": "template",
+        "template": {
+            "name": "tempalte1",
+            "language": {
+            "code": "en"
+            },
+            "components": [
+            {
+                "type": "header",
+                "parameters": [
+                {
+                    "type": "text",
+                    "text": req.body.customerData.insured_name
+                }
+                ]
+            }
+            ]
+        }
+        });
+
+        let config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://graph.facebook.com/v17.0/103303639487426/messages',
+        headers: { 
+            'Authorization': 'Bearer EAALkV5qUSzEBAGTYQgeZCjdBpCptoFSRr9xKZB2nX9Ct2yZBRs6sCIdPkbREG8XK9Co6KZANNgbIx6S34xasiEzoQDVv1nvIByVPlJXBAxYUZB0Lh89rXeIXtqgBKfh6Qmq5BHVdbZAhID0tykZCSOJBesJY4E5AR8ZBhkuLx1i4KEgBNXl7RnYMib2UEJ4LvIZAwabTKcLV4kwZDZD', 
+            'Content-Type': 'application/json'
+        },
+        data : data
+        };
+
+        axios.request(config)
+        .then((response) => {
+        console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+        console.log(error);
+        });
+
+});
