12a2a6ec325ff7d8bc0285b829b6a231f700affc
rishabparekh14 <rishabparekh14@gmail.com>
2023-08-07 17:18:19 -0700

    Check the button response code added

--- middleware/whatsappWebhooks.js
+++ middleware/whatsappWebhooks.js
@@ -34,7 +34,8 @@ exports.receiveReplyHook = (req, res) => {
       let phon_no_id =
         body_param.entry[0].changes[0].value.metadata.phone_number_id;
       let from = body_param.entry[0].changes[0].value.messages[0].from;
-      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+      let msg_body =
+        body_param.entry[0].changes[0].value.messages[0].button.text;
 
       console.log('phone number ' + phon_no_id);
       console.log('from ' + from);
